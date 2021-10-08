fc73283660b2e374822e280e5d82a9bfcbfdedd4
jungheajae <facerainbow@naver.com>
2021-10-08 18:33:20 +0900

    second commit

--- javascript/04-returns.js
+++ javascript/04-returns.js
@@ -2,3 +2,13 @@ const age = 96;
 function calculateKrAge(ageOfForeigner) {
   ageOfForeigner + 2;
 }
+
+const calculator = {
+  plus: function (a, b) {
+    return a + b;
+  },
+};
+
+const plusResult = calculator.plus(2, 3);
+
+console.log(plusResult);
